9317f22dfa4bd7b234e716fe7a86992034ddcafb
pandey-ankit <ankit4u123@gmail.com>
2025-12-13 02:35:22 +0000

    initial repo

---
+++ src/main/java/com/ankit/jobtracker/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.ankit.jobtracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Job Tracker Backend is running";
+    }
+}
